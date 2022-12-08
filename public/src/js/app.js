b977c52c966709f10c0d680dec5e0537fba9fd9c
Luka Jovanovic <luka.jovanovic.1995@gmail.com>
2022-12-08 11:06:41 +0100

    Add polyfills for Promises and fetch

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,9 +1,17 @@
 let deferredPrompt;
 
+if (!window.Promise) {
+  window.Promise = Promise;
+}
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw.js').then(() => {
-    console.log('serviceWorker registered');
-  });
+  navigator.serviceWorker
+    .register('/sw.js')
+    .then(() => {
+      console.log('serviceWorker registered');
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 }
 
 window.addEventListener('beforeinstallprompt', (e) => {
