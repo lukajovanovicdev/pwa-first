39475c35b1290cbb33b69688d303079308c69ee4
Luka Jovanovic <luka.jovanovic.1995@gmail.com>
2022-12-08 17:58:44 +0100

    Cache all initial files

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,23 @@
 self.addEventListener('install', (e) => {
   console.log('[SW] Installing event');
+  e.waitUntil(
+    caches.open('static').then((cache) => {
+      console.log('[SW] Pre-caching');
+      cache.addAll([
+        '/',
+        '/index.html',
+        '/src/js/app.js',
+        '/src/js/feed.js',
+        '/src/js/material.min.js',
+        'src/css/app.css',
+        'src/css/feed.css',
+        'src/images/main-image.jpg',
+        'https://fonts.googleapis.com/css?family=Roboto:400,700',
+        'https://fonts.googleapis.com/icon?family=Material+Icons',
+        'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css',
+      ]);
+    })
+  );
 });
 
 self.addEventListener('activate', (e) => {
@@ -8,6 +26,13 @@ self.addEventListener('activate', (e) => {
 });
 
 self.addEventListener('fetch', (e) => {
-  console.log('[SW] Fetching event', e);
-  e.respondWith(fetch(e.request));
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      if (response) {
+        return response;
+      } else {
+        return fetch(e.request);
+      }
+    })
+  );
 });
