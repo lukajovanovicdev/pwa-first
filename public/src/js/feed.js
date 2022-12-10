6f0cfc7d1e736947be3eebd1a53c5e73831c20d3
Luka Jovanovic <luka.jovanovic.1995@gmail.com>
2022-12-10 16:51:12 +0100

    Cache on demand

--- public/src/js/feed.js
+++ public/src/js/feed.js
@@ -30,6 +30,15 @@ shareImageButton.addEventListener('click', openCreatePostModal);
 
 closeCreatePostModalButton.addEventListener('click', closeCreatePostModal);
 
+// const onSaveButtonClicked = (e) => {
+//   console.log('Clicked');
+//   if ('caches' in window)
+//     caches.open('user-requested').then((cache) => {
+//       cache.add('https://httpbin.org/get');
+//       cache.add('/src/images/sf-boat.jpg');
+//     });
+// };
+
 function createCard() {
   var cardWrapper = document.createElement('div');
   cardWrapper.className = 'shared-moment-card mdl-card mdl-shadow--2dp';
@@ -48,6 +57,10 @@ function createCard() {
   cardSupportingText.className = 'mdl-card__supporting-text';
   cardSupportingText.textContent = 'In San Francisco';
   cardSupportingText.style.textAlign = 'center';
+  // var cardSaveButton = document.createElement('button');
+  // cardSaveButton.textContent = 'Save';
+  // cardSaveButton.addEventListener('click', onSaveButtonClicked);
+  // cardSupportingText.appendChild(cardSaveButton);
   cardWrapper.appendChild(cardSupportingText);
   componentHandler.upgradeElement(cardWrapper);
   sharedMomentsArea.appendChild(cardWrapper);
