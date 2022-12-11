8e9b2baa5a5a2549da0e179fe4be5726c60125a3
Luka Jovanovic <luka.jovanovic.1995@gmail.com>
2022-12-11 01:10:05 +0100

    Apply cache only

--- public/sw.js
+++ public/sw.js
@@ -1,25 +1,26 @@
-let CACHE_STATIC_NAME = 'static-v1';
-let CACHE_DYNAMIC_NAME = 'dynamic-v2';
+const CACHE_STATIC_NAME = 'static-v1';
+const CACHE_DYNAMIC_NAME = 'dynamic-v2';
+const STATIC_FILES = [
+  '/',
+  '/index.html',
+  '/offline.html',
+  '/src/js/app.js',
+  '/src/js/feed.js',
+  '/src/js/material.min.js',
+  'src/css/app.css',
+  'src/css/feed.css',
+  'src/images/main-image.jpg',
+  'https://fonts.googleapis.com/css?family=Roboto:400,700',
+  'https://fonts.googleapis.com/icon?family=Material+Icons',
+  'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css',
+];
 
 self.addEventListener('install', (e) => {
   console.log('[SW] Installing event');
   e.waitUntil(
     caches.open(CACHE_STATIC_NAME).then((cache) => {
       console.log('[SW] Pre-caching');
-      cache.addAll([
-        '/',
-        '/index.html',
-        '/offline.html',
-        '/src/js/app.js',
-        '/src/js/feed.js',
-        '/src/js/material.min.js',
-        'src/css/app.css',
-        'src/css/feed.css',
-        'src/images/main-image.jpg',
-        'https://fonts.googleapis.com/css?family=Roboto:400,700',
-        'https://fonts.googleapis.com/icon?family=Material+Icons',
-        'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css',
-      ]);
+      cache.addAll(STATIC_FILES);
     })
   );
 });
@@ -54,6 +55,8 @@ self.addEventListener('fetch', (e) => {
         })
       )
     );
+  } else if (new RegExp('\\b' + STATIC_FILES.join('\\b|\\b') + '\\b').test(e.request.url)) {
+    e.respondWith(caches.match(e.request));
   } else {
     e.respondWith(
       caches.match(e.request).then((response) => {
